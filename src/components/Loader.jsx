5b7d875671c4cc2a5b1d86742225496f8579062e
Mariano Pavone <mariano0891@gmail.com>
2024-09-14 14:55:07 -0300

    added the loader component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,10 @@
-
+import PropagateLoader from 'react-spinners/PropagateLoader'
 
 export const Loader = () => {
     console.log ("esta funcionando")
   return (
-    <div>Loader</div>
+    <div className='my-20 flex justify-center'>
+      <PropagateLoader />
+    </div>
   )
 }
